512759d542e440a5b7920c91c972519e13a173fb
JackBorer <jackborer@utexas.edu>
2021-04-07 22:04:19 -0500

    Struct struct control and overidden ostream

--- include/vslam_types.h
+++ include/vslam_types.h
@@ -22,7 +22,8 @@ struct VisionFeature {
   // Camera pixel location of feature.
   Eigen::Vector2f const pixel;
   // Estimated 3d location in the camera frame - not used directly in
-  // structureless slam but left here for completeness and later use
+  // structureless slam but left here for completeness and later use - the only
+  // non const member
   Eigen::Vector3f point3d;
   // Default constructor: removed - should only be constructed when all parts
   // are available - allows us to enforce all parts of the feature being const -
@@ -32,11 +33,21 @@ struct VisionFeature {
   VisionFeature(const uint64_t feature_idx,
                 const T descriptor,
                 const Eigen::Vector2f& pixel,
-                const Eigen::Vector3f& point3d)
+                const Eigen::Vector3f& point3d = Eigen::Vector3f())
       : feature_idx(feature_idx),
         descriptor(descriptor),
         pixel(pixel),
         point3d(point3d) {}
+  // Convenience override of ostream - Each descriptor will need to have its own
+  // overridden operator
+  friend std::ostream& operator<<(std::ostream& o,
+                                  const vslam_types::VisionFeature<T>& f) {
+    o << "feature_idx: " << f.feature_idx << "\tdescriptor: " << f.descriptor
+      << "\tpixel: " << f.pixel.x() << " " << f.pixel.y()
+      << "\tpoint3d: " << f.point3d.x() << " " << f.point3d.y() << " "
+      << f.point3d.z() << std::endl;
+    return o;
+  }
 };
 
 // Templated for the type of descriptor used
@@ -44,14 +55,28 @@ template <typename T>
 struct VisionFeatureTrack {
   // Index of this feature track - should never be changed once created
   uint64_t const feature_idx;
-  // The track of feature matches
+
+ private:
+  // The track of feature matches - private - only allow controlled feature
+  // addition and no feature subtraction
   std::vector<VisionFeature<T>> track;
+
+ public:
+  // Add feature to end of feature track
+  void addFeature(VisionFeature<T> feature) {
+    track.push_back(feature);
+    return;
+  };
+  // Return reference to the feature track vector that doesn't allow you to edit
+  // the feature track
+  std::vector<VisionFeature<T>> const& getTrack() const { return track; }
   // Default constructor: removed - must construct feature track with const
   // feature_idx
   VisionFeatureTrack() = delete;
   // Convenience constructor: initialize everything.
-  VisionFeatureTrack(uint64_t const feature_idx,
-                     std::vector<VisionFeature<T>> track)
+  VisionFeatureTrack(
+      uint64_t const feature_idx,
+      std::vector<VisionFeature<T>> track = std::vector<VisionFeature<T>>())
       : feature_idx(feature_idx), track(track){};
 };
 
