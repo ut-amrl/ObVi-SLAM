92e7b7d777c5762f1ca2e7a93156519cd57b5410
JackBorer <jackborer@utexas.edu>
2021-04-08 16:20:20 -0500

    Remove const becasue it make assignment impossible

--- include/vslam_types.h
+++ include/vslam_types.h
@@ -1,6 +1,8 @@
 #ifndef __VSLAM_TYPES_H__
 #define __VSLAM_TYPES_H__
 
+#include <assert.h>
+
 #include <iostream>
 #include <utility>
 #include <vector>
@@ -15,23 +17,20 @@ template <typename T>
 struct VisionFeature {
   // Index of this feature - same as the index of the feature track of which
   // this is a part - stored here for redundancy
-  uint64_t const feature_idx;
+  uint64_t feature_idx;
   // Index of the frame this feature was acquired in
-  uint64_t const frame_idx;
+  uint64_t frame_idx;
   // The descriptor of the feature from the frame it was imaged in - the
-  // descriptor will potentially change slightly over the course of the feature
-  // track
-  T const descriptor;
+  // descriptor will potentially change slightly over the course of the
+  // feature track
+  T descriptor;
   // Camera pixel location of feature.
-  Eigen::Vector2f const pixel;
+  Eigen::Vector2f pixel;
   // Estimated 3d location in the camera frame - not used directly in
-  // structureless slam but left here for completeness and later use - the only
-  // non const member
+  // structureless slam but left here for completeness and later use
   Eigen::Vector3f point3d;
-  // Default constructor: removed - should only be constructed when all parts
-  // are available - allows us to enforce all parts of the feature being const -
-  // except for the point3d location which we may use later/optimize
-  VisionFeature() = delete;
+  // Default constructor: do nothing.
+  VisionFeature();
   // Convenience constructor: initialize everything.
   VisionFeature(uint64_t const feature_idx,
                 uint64_t const frame_idx,
@@ -43,15 +42,25 @@ struct VisionFeature {
         descriptor(descriptor),
         pixel(pixel),
         point3d(point3d) {}
-  // Convenience override of ostream - Each descriptor will need to have its own
-  // overridden operator
-  friend std::ostream& operator<<(std::ostream& o,
-                                  const vslam_types::VisionFeature<T>& f) {
+  // Convenience override of ostream - Each descriptor will need to have its
+  // own overridden operator
+  friend std::ostream& operator<<(std::ostream& o, const VisionFeature<T>& f) {
     o << "feature_idx: " << f.feature_idx << "\tframe_idx: " << f.frame_idx
       << "\tdescriptor: " << f.descriptor << "\tpixel: " << f.pixel.x() << " "
       << f.pixel.y() << "\tpoint3d: " << f.point3d.x() << " " << f.point3d.y()
       << " " << f.point3d.z() << std::endl;
     return o;
+  }
+  // Override of less than operator - compares two features based on the
+  // frame_idx - assumes that frame_idxs are sequential and always increasing-
+  // this overload allows us to sort feature tracks
+  friend bool operator<(VisionFeature<T> const& lhs,
+                        VisionFeature<T> const& rhs) {
+    // Assert that features being compared are in same track - comparing
+    // features across tracks in this manner doesn't have a very intuitive
+    // meaning
+    assert(lhs.feature_idx == rhs.feature_idx);
+    return lhs.frame_idx < rhs.frame_idx ? true : false;
   }
 };
 
@@ -59,8 +68,8 @@ struct VisionFeature {
 template <typename T>
 struct VisionFeatureTrack {
   // Index of this feature track - should never be changed once created - each
-  // member feature will also have this redundantly
-  uint64_t const feature_idx;
+  // member feature in the track will also have this redundantly
+  uint64_t feature_idx;
 
  private:
   // The track of feature matches - private - only allow controlled feature
@@ -73,8 +82,13 @@ struct VisionFeatureTrack {
     track.push_back(feature);
     return;
   };
-  // Return reference to the feature track vector that doesn't allow you to edit
-  // the feature track
+  // Sort feature track so frame_idxs are in ascending order
+  void sort() {
+    std::sort(track.begin(), track.end());
+    return;
+  }
+  // Return reference to the feature track vector that doesn't allow you to
+  // edit the feature track
   std::vector<VisionFeature<T>> const& getTrack() const { return track; }
   // Default constructor: removed - must construct feature track with const
   // feature_idx
@@ -84,15 +98,36 @@ struct VisionFeatureTrack {
       uint64_t const feature_idx,
       std::vector<VisionFeature<T>> track = std::vector<VisionFeature<T>>())
       : feature_idx(feature_idx), track(track){};
+  // Convenience constructor: initialize with new seed feature
+  VisionFeatureTrack(VisionFeature<T> feature)
+      : feature_idx(feature.feature_idx) {
+    track.push_back(feature);
+  }
 };
 
 // Templated for the type of descriptor used
 template <typename T>
 struct TrackDatabase {
-  // All frame IDs - each tracks individual frame IDs will be a subset of these
+  // All frame IDs - each tracks individual frame IDs will be a subset of
+  // these
   std::vector<uint64_t> frame_idxs;
   // All feature tracks
   std::vector<VisionFeatureTrack<T>> feature_tracks;
+  // Add feature to track database
+  void addFeature(VisionFeature<T> feature) {
+    // If feature track exists add the feature
+    for (auto& ft : feature_tracks) {
+      if (ft.feature_idx == feature.feature_idx) {
+        ft.addFeature(feature);
+        return;  // Exit early if we already found the track we were looking
+                 // for
+      }
+    }
+    // If feature track doesn't exist seed a new one with the feature
+    feature_tracks.push_back(VisionFeatureTrack<int>(feature));
+
+    return;
+  }
   // Default constructor: do nothing.
   TrackDatabase(){};
   // Convenience constructor: initialize everything.
