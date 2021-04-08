0e2f10a5f47c6c014e25790bad5f7570722fba56
JackBorer <jackborer@utexas.edu>
2021-04-08 16:20:27 -0500

    Demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,42 @@
 #include <gflags/gflags.h>
 #include <glog/logging.h>
 
+#include <iostream>
+
+#include "vslam_io.h"
 #include "vslam_types.h"
 
-int main(int argc, char** argv) { return 0; }
+DEFINE_string(
+    data_path,
+    "",
+    "Path to folder containing the files with frames, poses, and labeled "
+    "keypoints for each image");
+
+using std::cout;
+using std::endl;
+
+int main(int argc, char** argv) {
+  google::InitGoogleLogging(argv[0]);
+  google::ParseCommandLineFlags(&argc, &argv, true);
+
+  FLAGS_logtostderr = true;  // Don't log to disk - log to terminal
+
+  // Make empty unstructured slam problem
+  vslam_types::UTSLAMProblem<int> prob;
+  // Load unstructured slam problem
+  vslam_io::LoadUTSLAMProblem(FLAGS_data_path, &prob);
+
+  // Print poses to terminal for display
+  for (const auto& pose : prob.robot_poses) {
+    cout << pose << endl;
+  }
+
+  // Print feature tracks to terminal for display
+  for (const auto& track : prob.track_database.feature_tracks) {
+    for (const auto& feature : track.getTrack()) {
+      cout << feature << endl;
+    }
+  }
+
+  return 0;
+}
