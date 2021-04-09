75d9600de06f2f1542b7c4991952b9871a0fce0a
JackBorer <jackborer@utexas.edu>
2021-04-09 14:49:43 -0500

    Added doc - @mandi1267 what doc style you like?

--- include/vslam_io.h
+++ include/vslam_io.h
@@ -5,9 +5,26 @@
 
 namespace vslam_io {
 
-// Loads UTSLAM problem from file -
+/**
+ * \brief   Loads a unstuctured slam problem from file.
+ *
+ * \details This function loads an unstructured slam problem for a directory
+ * containing data files. The first data file is am integer frame ID, the second
+ * line is the frame position in the global frame (x, y, z, qx, qy, qz, qw). All
+ * other lines are images keypoints (ID, x_pixel, y_pixel)
+ *
+ * \note    There should be no non data .txt files in the data directory -
+ * potentially the system could confuse these for a valid data file
+ *
+ * \param[in]     data_path    Absolute or relative path to the data directory
+ * \param[in,out] prob    The vslam_types::UTSLAMProblem() that the data will be
+ * loaded into - this essentially consists of robot/frame poses and feature
+ * tracks
+ *
+ * \return        void
+ */
 void LoadUTSLAMProblem(const std::string data_path,
-                       vslam_types::UTSLAMProblem& prob_ptr);
+                       vslam_types::UTSLAMProblem& prob);
 }  // namespace vslam_io
 
 #endif  // __VSLAM_IO_H__
