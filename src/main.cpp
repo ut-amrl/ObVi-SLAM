95440e5c74b5e8751822955a486e2aa6166d839e
JackBorer <jackborer@utexas.edu>
2021-04-16 10:30:47 -0500

    Hookup front end to back end

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 
+#include "slam_backend_solver.h"
+#include "slam_solver_optimizer_params.h"
 #include "vslam_io.h"
 #include "vslam_types.h"
 
@@ -19,7 +21,7 @@ DEFINE_string(calibration_path,
 using std::cout;
 using std::endl;
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   google::InitGoogleLogging(argv[0]);
   google::ParseCommandLineFlags(&argc, &argv, true);
 
@@ -35,19 +37,48 @@ int main(int argc, char** argv) {
   vslam_io::LoadCameraCalibration(FLAGS_calibration_path, K);
 
   // Print poses to terminal for display
-  for (const auto& pose : prob.robot_poses) {
+  for (const auto &pose : prob.robot_poses) {
     cout << pose << endl;
   }
 
   // Print feature tracks to terminal for display
-  for (const auto& ft : prob.tracks) {
-    for (const auto& feature : ft.second.track) {
+  for (const auto &ft : prob.tracks) {
+    for (const auto &feature : ft.second.track) {
       cout << feature << endl;
     }
   }
 
   // Print camera intrinsics to terminal for display
   cout << K << endl;
+
+  // Make intrinsics and unit camera extrinsics
+  vslam_types::CameraIntrinsics intrinsics{K};
+  vslam_types::CameraExtrinsics extrinsics{Eigen::Vector3f(0, 0, 0),
+                                           Eigen::Quaternionf(1, 0, 0, 0)};
+
+  // Solve
+  vslam_solver::SLAMSolverOptimizerParams optimizer_params;
+  vslam_solver::SLAMSolver solver(optimizer_params);
+
+  // These are the poses that are going to be optimized
+  std::vector<vslam_types::RobotPose> answer(prob.robot_poses);
+
+  std::function<void(
+      const vslam_types::UTSLAMProblem<vslam_types::VisionFeatureTrack> &,
+      const vslam_types::CameraIntrinsics &,
+      const vslam_types::CameraExtrinsics &,
+      const vslam_solver::SLAMSolverOptimizerParams &,
+      ceres::Problem &,
+      std::vector<vslam_solver::SLAMNode> *)>
+      structureless_vision_constraint_adder =
+          vslam_solver::AddStructurelessVisionFactors;
+
+  solver.SolveSLAM<vslam_types::VisionFeatureTrack>(
+      intrinsics,
+      extrinsics,
+      prob,
+      structureless_vision_constraint_adder,
+      answer);
 
   return 0;
 }
