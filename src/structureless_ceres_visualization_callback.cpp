64dcbdc136f8180c5c4077eb3ce177b0f3950695
JackBorer <jackborer@utexas.edu>
2021-04-26 15:16:27 -0500

    Pairwise fundamental matrix

--- src/structureless_ceres_visualization_callback.cpp
+++ src/structureless_ceres_visualization_callback.cpp
@@ -1,5 +1,6 @@
 #include <glog/logging.h>
 #include <structureless_ceres_visualization_callback.h>
+#include <vslam_types.h>
 
 namespace vslam_viz {
 
@@ -20,13 +21,41 @@ ceres::CallbackReturnType StructurelessCeresVisualizationCallback::operator()(
   // DO NOT MODIFY THESE -- they are being updated during optimization
   std::vector<vslam_types::SLAMNode> nodes = *slam_nodes_;
 
-  for (int i = 0; i < nodes.size(); i++) {
-    // TODO this can be removed once we add visualization -- just to demonstrate
-    // that it is called
-    const vslam_types::SLAMNode node = nodes[i];
-    LOG(INFO) << "Node " << i << ": " << node.pose[0] << ", " << node.pose[1]
-              << ", " << node.pose[2] << ", " << node.pose[3] << ", "
-              << node.pose[4] << ", " << node.pose[5];
+  Eigen::Affine3d cam_to_robot_tf =
+      Eigen::Translation3d(extrinsics_.translation.cast<double>()) *
+      extrinsics_.rotation.cast<double>();
+
+  LOG(INFO) << "Callback Viz";
+  for (int i = 0; i < nodes.size() - 1; i++) {
+    // Convert pose 1 to tf
+    const vslam_types::SLAMNode node1 = nodes[i];  // i
+    Eigen::Transform<double, 3, Eigen::Affine> first_robot_pose_in_world =
+        vslam_types::PoseArrayToAffine<double>(&(node1.pose[3]),
+                                               &(node1.pose[0]));
+
+    // Convert pose 2 to tf
+    const vslam_types::SLAMNode node2 = nodes[i + 1];  // i+1
+    Eigen::Transform<double, 3, Eigen::Affine> second_robot_pose_in_world =
+        vslam_types::PoseArrayToAffine<double>(&(node2.pose[3]),
+                                               &(node2.pose[0]));
+
+    Eigen::Transform<double, 3, Eigen::Affine> cam_1_to_cam_2_mat =
+        cam_to_robot_tf.inverse() * first_robot_pose_in_world.inverse() *
+        second_robot_pose_in_world * cam_to_robot_tf;
+
+    // Extract Tx and R from cam_1_to_cam_2_mat
+    Eigen::Matrix<double, 3, 1> t_vec = cam_1_to_cam_2_mat.translation();
+    Eigen::Matrix<double, 3, 3> t_cross;  // skew symmetric
+    t_cross << (0.0), -t_vec(2), t_vec(1), t_vec(2), (0.0), -t_vec(0),
+        -t_vec(1), t_vec(0), (0.0);
+    Eigen::Matrix<double, 3, 3> rotation = cam_1_to_cam_2_mat.linear();
+
+    Eigen::Matrix<double, 3, 3> essential_mat = t_cross * rotation;
+
+    // Convert to fundamental matrix with K
+    Eigen::Matrix<double, 3, 3> fundamental_mat =
+        intrinsics_.camera_mat.inverse().inverse().cast<double>() *
+        essential_mat * intrinsics_.camera_mat.inverse().cast<double>();
   }
 
   return ceres::SOLVER_CONTINUE;
