cf742a4535f5c2985fa2f04c548e15e39c63be1a
Amanda Adkins <mandi1267@gmail.com>
2022-01-10 11:33:58 -0600

    Add implementation of shape prior factor

---
+++ src/ellipsoid_estimator_main.cpp
@@ -0,0 +1,14 @@
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+#include <shape_prior_factor.h>
+
+int main(int argc, char **argv) {
+  google::InitGoogleLogging(argv[0]);
+  google::ParseCommandLineFlags(&argc, &argv, true);
+
+  FLAGS_logtostderr = true;  // Don't log to disk - log to terminal
+
+  // TODO fill in with ellipsoid estimation code (step 2)
+
+  return 0;
+}
