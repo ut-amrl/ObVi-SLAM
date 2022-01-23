5a290da9e3b0f834f26fac040ddcbfe7b18f00bc
Amanda Adkins <mandi1267@gmail.com>
2022-01-23 15:17:11 -0600

    Work on building test problem -- no bounding boxes exist, so need to debug that

--- src/ellipsoid_estimator_main.cpp
+++ src/ellipsoid_estimator_main.cpp
@@ -1,9 +1,10 @@
-#include <bounding_box_factor.h>
 #include <gflags/gflags.h>
 #include <glog/logging.h>
 #include <object_slam_backend_solver.h>
 #include <shape_prior_factor.h>
+#include <synthetic_problem/synthetic_problem_construction_utils.h>
 #include <synthetic_problem/synthetic_problem_generator.h>
+#include <visual_slam_ceres_visualization_callback.h>
 
 int main(int argc, char **argv) {
   google::InitGoogleLogging(argv[0]);
@@ -13,9 +14,7 @@ int main(int argc, char **argv) {
 
   FLAGS_logtostderr = true;  // Don't log to disk - log to terminal
 
-  // TODO fill in with ellipsoid estimation code (step 2)
-
-  std::vector<vslam_types::EllipsoidEstimate> ground_truth_ellipsoids; 
+  std::vector<vslam_types::EllipsoidEstimate> ground_truth_ellipsoids;
   vslam_types::EllipsoidEstimate ellipsoid_est(
       Eigen::Vector3f(0, 0, 0),
       Eigen::AngleAxisf(0, Eigen::Vector3f(1, 0, 0)),
@@ -24,10 +23,46 @@ int main(int argc, char **argv) {
       0);
   ground_truth_ellipsoids.emplace_back(ellipsoid_est);
 
-  std::vector<vslam_types::RobotPose> ground_truth_robot_poses;  // TODO
+  std::vector<vslam_types::RobotPose> ground_truth_robot_poses;
+  ground_truth_robot_poses.emplace_back(vslam_types::RobotPose(
+      ground_truth_robot_poses.size() - 1,
+      Eigen::Vector3f(20, 0, 0),
+      synthetic_problem::createAngleAxisFromYaw(M_PI_2)));
+  ground_truth_robot_poses.emplace_back(
+      vslam_types::RobotPose(ground_truth_robot_poses.size() - 1,
+                             Eigen::Vector3f(0, 20, 0),
+                             synthetic_problem::createAngleAxisFromYaw(M_PI)));
+  ground_truth_robot_poses.emplace_back(vslam_types::RobotPose(
+      ground_truth_robot_poses.size() - 1,
+      Eigen::Vector3f(-20, 0, 0),
+      synthetic_problem::createAngleAxisFromYaw(-M_PI_2)));
+  ground_truth_robot_poses.emplace_back(
+      vslam_types::RobotPose(ground_truth_robot_poses.size() - 1,
+                             Eigen::Vector3f(0, -20, 0),
+                             synthetic_problem::createAngleAxisFromYaw(0)));
+
   std::unordered_map<vslam_types::CameraId, vslam_types::CameraIntrinsics>
-      intrinsics;                                            // TODO
-  Eigen::Vector4f bounding_box_std_devs;                     // TODO
+      intrinsics;
+  vslam_types::CameraIntrinsics intrinsics_for_both;
+
+  float fx = 585;  // TODO consider changing
+  float fy = 585;
+  float cx = 320;
+  float cy = 240;
+  intrinsics_for_both.camera_mat.setIdentity();
+  intrinsics_for_both.camera_mat(0, 0) = fx;
+  intrinsics_for_both.camera_mat(1, 1) = fy;
+  intrinsics_for_both.camera_mat(0, 2) = cx;
+  intrinsics_for_both.camera_mat(1, 2) = cy;
+  intrinsics[0] = intrinsics_for_both;
+  intrinsics[1] = intrinsics_for_both;
+
+  Eigen::Vector4f bounding_box_std_devs;  // TODO
+  bounding_box_std_devs(0) = 1e-5;
+  bounding_box_std_devs(1) = 1e-5;
+  bounding_box_std_devs(2) = 1e-5;
+  bounding_box_std_devs(3) = 1e-5;
+
   Eigen::Matrix<float, 6, 1> ellipsoid_pose_estimate_noise;  // TODO
   Eigen::Matrix<float, 6, 1> robot_pose_std_devs;            // TODO
 
@@ -44,6 +79,7 @@ int main(int argc, char **argv) {
   Eigen::Quaternionf extrinsics_orientation =
       Eigen::Quaternionf(0.5, 0.5, -0.5, 0.5)
           .inverse();  // [0 -1 0; 0 0 -1; 1 0 0]^-1
+  LOG(INFO) << "Extrinsics orientation " << extrinsics_orientation.w() << ", " << extrinsics_orientation.x() << ", " << extrinsics_orientation.y() << ", " << extrinsics_orientation.z();
   extrinsics[0] = {Eigen::Vector3f(0, 0, 0), extrinsics_orientation};
   // TODO Is translation before or after rotation? (i.e. should we use camera
   // frame axes or robot frame)
@@ -55,7 +91,7 @@ int main(int argc, char **argv) {
   std::pair<vslam_types::UTObjectSLAMProblem<
                 vslam_types::StructuredVisionFeatureTrack>,
             vslam_solver::ObjectSlamProblemParams>
-      slam_problem_and_params =
+      slam_problem_and_params = synthetic_problem::
           createEllipsoidOnlySyntheticProblemFromEllipsoidsAndCameraPoses<
               vslam_types::StructuredVisionFeatureTrack>(
               ground_truth_ellipsoids,
@@ -69,6 +105,62 @@ int main(int argc, char **argv) {
               bounding_box_detection_success_rate,
               ellipsoid_visibility_params,
               random_gen);
+
+  LOG(INFO) << "Created problem with " << slam_problem_and_params.first.bounding_boxes.size() << " bounding box detections";
+
+  vslam_solver::SLAMSolverOptimizerParams optimizer_params;
+  vslam_solver::StructuredObjectSlamProblemParams problem_params;
+  problem_params.object_slam_params = slam_problem_and_params.second;
+
+  std::vector<vslam_types::EllipsoidEstimate> updated_ellipsoid_estimates;
+
+  std::function<std::vector<vslam_types::VisionFeature>(
+      const vslam_types::StructuredVisionFeatureTrack &)>
+      feature_retriever =
+          [](const vslam_types::StructuredVisionFeatureTrack &feature_track) {
+            return feature_track.feature_track.track;
+          };
+
+  // Got the casting solution from
+  // https://stackoverflow.com/questions/30393285/stdfunction-fails-to-distinguish-overloaded-functions
+  typedef std::shared_ptr<vslam_viz::VisualSlamCeresVisualizationCallback<
+      vslam_types::StructuredVisionFeatureTrack>> (*funtype)(
+      const vslam_types::UTSLAMProblem<
+          vslam_types::StructuredVisionFeatureTrack> &,
+      const std::function<std::vector<vslam_types::VisionFeature>(
+          const vslam_types::StructuredVisionFeatureTrack &)> &,
+      const std::vector<vslam_types::RobotPose> &,
+      std::vector<vslam_types::SLAMNode> *);
+  funtype func = vslam_viz::VisualSlamCeresVisualizationCallback<
+      vslam_types::StructuredVisionFeatureTrack>::create;
+
+  std::function<std::shared_ptr<ceres::IterationCallback>(
+      const vslam_types::UTSLAMProblem<
+          vslam_types::StructuredVisionFeatureTrack> &,
+      const std::function<std::vector<vslam_types::VisionFeature>(
+          const vslam_types::StructuredVisionFeatureTrack &)> &,
+      const std::vector<vslam_types::RobotPose> &,
+      std::vector<vslam_types::SLAMNode> *)>
+      unbound_callback_creator = func;
+
+  std::function<std::shared_ptr<ceres::IterationCallback>(
+      const vslam_types::UTSLAMProblem<
+          vslam_types::StructuredVisionFeatureTrack> &,
+      std::vector<vslam_types::SLAMNode> *)>
+      callback_creator = std::bind(unbound_callback_creator,
+                                   std::placeholders::_1,
+                                   feature_retriever,
+                                   ground_truth_robot_poses,
+                                   std::placeholders::_2);
+
+  bool slam_convergence_result =
+      findEllipsoidEstimates(slam_problem_and_params.first,
+                             optimizer_params,
+                             callback_creator,
+                             problem_params,
+                             updated_ellipsoid_estimates);
+
+  LOG(INFO) << "Convergence result " << slam_convergence_result;
 
   return 0;
 }
