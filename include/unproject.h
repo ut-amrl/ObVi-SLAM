b862d77dd1b17f33ea56e004d9bbb63150c2d038
tiejean <taijingchen99@gmail.com>
2022-01-25 17:03:41 -0600

    fix depth typo

--- include/unproject.h
+++ include/unproject.h
@@ -19,7 +19,7 @@ Vector3f Unproject(const Vector2f& image_feature,
                    const CameraExtrinsics& extrinsics,
                    const RobotPose& robot_pose,
                    const float depth) {
-	Vector3f point_in_cam  = intrinsics.camera_mat.inverse() * Vector3f(image_feature.x(), image_feature.y(), 1);
+	Vector3f point_in_cam  = intrinsics.camera_mat.inverse() * Vector3f(image_feature.x(), image_feature.y(), depth);
 	AngleAxisf angle(extrinsics.rotation);
 	Affine3f camera_to_robot = Affine3f::Identity();
 	camera_to_robot.translate(extrinsics.translation);
@@ -29,7 +29,7 @@ Vector3f Unproject(const Vector2f& image_feature,
 	// Vector3f point_in_robot = PoseArrayToAffine(&angle, &extrinsics.translation).inverse() * point_in_cam;
 	Vector3f point_in_world = robot_pose.RobotToWorldTF() * point_in_robot;
 	if (0) {
-		printf("measurement: %.2f,%.2f\n", image_feature.x(), image_feature.y());
+		printf("measurement: %.2f,%.2f; depth: %.2f\n", image_feature.x(), image_feature.y(), depth);
 		printf("point_in_cam: %.2f,%.2f,%.2f\n", point_in_cam.x(), point_in_cam.y(), point_in_cam.z());
 		printf("point_in_robot: %.2f,%.2f,%.2f\n", point_in_robot.x(), point_in_robot.y(), point_in_robot.z());
 		printf("point_in_world: %.2f,%.2f,%.2f\n", point_in_world.x(), point_in_world.y(), point_in_world.z());
