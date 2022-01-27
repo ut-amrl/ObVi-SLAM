c493b5ea6307854e19f96de24688ee6830d20ac2
tiejean <taijingchen99@gmail.com>
2022-01-26 20:57:23 -0600

    fix indentation in unproject

--- include/unproject.h
+++ include/unproject.h
@@ -19,23 +19,24 @@ Vector3f Unproject(const Vector2f& image_feature,
                    const CameraExtrinsics& extrinsics,
                    const RobotPose& robot_pose,
                    const float depth) {
-	Vector3f point_in_cam  = intrinsics.camera_mat.inverse() * (Vector3f(image_feature.x(), image_feature.y(), 1) * depth);
-	AngleAxisf angle(extrinsics.rotation);
-	Affine3f camera_to_robot = Affine3f::Identity();
-	camera_to_robot.translate(extrinsics.translation);
-	camera_to_robot.rotate(extrinsics.rotation);
-	camera_to_robot = camera_to_robot.inverse();
-	Vector3f point_in_robot = camera_to_robot * point_in_cam;
-	// Vector3f point_in_robot = PoseArrayToAffine(&angle, &extrinsics.translation).inverse() * point_in_cam;
-	Vector3f point_in_world = robot_pose.RobotToWorldTF() * point_in_robot;
-	if (0) {
-		printf("measurement: %.2f,%.2f; depth: %.2f\n", image_feature.x(), image_feature.y(), depth);
-		printf("point_in_cam: %.2f,%.2f,%.2f\n", point_in_cam.x(), point_in_cam.y(), point_in_cam.z());
-		printf("point_in_robot: %.2f,%.2f,%.2f\n", point_in_robot.x(), point_in_robot.y(), point_in_robot.z());
-		printf("point_in_world: %.2f,%.2f,%.2f\n", point_in_world.x(), point_in_world.y(), point_in_world.z());
-		cout << endl;
-	}
-	return point_in_world;
+    Vector3f point_in_cam  = intrinsics.camera_mat.inverse() * Vector3f(image_feature.x(), image_feature.y(), 1);
+    point_in_cam = depth * point_in_cam;
+    AngleAxisf angle(extrinsics.rotation);
+    Affine3f camera_to_robot = Affine3f::Identity();
+    camera_to_robot.translate(extrinsics.translation);
+    camera_to_robot.rotate(extrinsics.rotation);
+    camera_to_robot = camera_to_robot.inverse();
+    Vector3f point_in_robot = camera_to_robot * point_in_cam;
+    // Vector3f point_in_robot = PoseArrayToAffine(&angle, &extrinsics.translation).inverse() * point_in_cam;
+    Vector3f point_in_world = robot_pose.RobotToWorldTF() * point_in_robot;
+    if (0) {
+        printf("measurement: %.2f,%.2f; depth: %.2f\n", image_feature.x(), image_feature.y(), depth);
+        printf("point_in_cam: %.2f,%.2f,%.2f\n", point_in_cam.x(), point_in_cam.y(), point_in_cam.z());
+        printf("point_in_robot: %.2f,%.2f,%.2f\n", point_in_robot.x(), point_in_robot.y(), point_in_robot.z());
+        printf("point_in_world: %.2f,%.2f,%.2f\n", point_in_world.x(), point_in_world.y(), point_in_world.z());
+        cout << endl;
+    }
+    return point_in_world;
 }
 
 void Unproject(const Vector2f& image_feature,
@@ -44,17 +45,17 @@ void Unproject(const Vector2f& image_feature,
                const RobotPose& robot_pose,
                const float depth,
                Vector3f* point_ptr) {
-	Vector3f& point = *point_ptr;
-	Vector3f point_in_cam  = intrinsics.camera_mat.inverse() * Vector3f(image_feature.x(), image_feature.y(), 1);
-	Affine3f camera_to_robot = Affine3f::Identity();
-	camera_to_robot.translate(extrinsics.translation);
-	camera_to_robot.rotate(extrinsics.rotation);
-	camera_to_robot = camera_to_robot.inverse();
-	Vector3f point_in_robot = camera_to_robot * point_in_cam;
-	// AngleAxisf angle(extrinsics.rotation);
-	// Vector3f point_in_robot = PoseArrayToAffine(const_cast<const AngleAxisf*> (&angle), &extrinsics.translation).inverse() * point_in_cam;
-	Vector3f point_in_world = robot_pose.RobotToWorldTF() * point_in_world;
-	point = point_in_world;
+    Vector3f& point = *point_ptr;
+    Vector3f point_in_cam  = intrinsics.camera_mat.inverse() * Vector3f(image_feature.x(), image_feature.y(), 1);
+    Affine3f camera_to_robot = Affine3f::Identity();
+    camera_to_robot.translate(extrinsics.translation);
+    camera_to_robot.rotate(extrinsics.rotation);
+    camera_to_robot = camera_to_robot.inverse();
+    Vector3f point_in_robot = camera_to_robot * point_in_cam;
+    // AngleAxisf angle(extrinsics.rotation);
+    // Vector3f point_in_robot = PoseArrayToAffine(const_cast<const AngleAxisf*> (&angle), &extrinsics.translation).inverse() * point_in_cam;
+    Vector3f point_in_world = robot_pose.RobotToWorldTF() * point_in_world;
+    point = point_in_world;
 }
 
 } // namespace unproject
