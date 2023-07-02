7725917ccbe92a9cd96ad4091ea80295f5767e08
Amanda Adkins <mandi1267@gmail.com>
2023-07-02 16:48:57 -0500

    Fix metrics generator

--- convenience_scripts/experiments/amanda_robofleet/eer_jackal_ouster/ut_vslam_metrics_generator.sh
+++ convenience_scripts/experiments/amanda_robofleet/eer_jackal_ouster/ut_vslam_metrics_generator.sh
@@ -10,6 +10,8 @@ results_root_directory=${root_data_dir}ut_vslam_results/
 trajectory_sequence_file_directory=${SLAM_DIR}sequences/
 lego_loam_out_root_dir=${root_data_dir}lego_loam_out/
 
+odometry_topic="/jackal_velocity_controller/odom"
+
 config_file_base_name="base4"
 sequence_file_base_name="amazon_0523_v0"
 
@@ -22,6 +24,7 @@ make && python3 src/evaluation/compute_metrics_for_ut_vslam.py \
     --orb_post_process_base_directory=${orb_post_process_base_directory} \
     --config_file_base_name=${config_file_base_name} \
     --results_root_directory=${results_root_directory} \
+    --odometry_topic ${odometry_topic} \
 #    --force_rerun_interpolator \
 #    --force_rerun_metrics_generator \
 #    --force_rerun_trajectory_formatter
