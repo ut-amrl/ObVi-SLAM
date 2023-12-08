101db42367d602400b8d3b32f627b3396fa243b0
Amanda Adkins <mandi1267@gmail.com>
2023-12-08 14:33:29 -0600

    Add more detailed timers for pose graph copy

--- include/analysis/cumulative_timer_constants.h
+++ include/analysis/cumulative_timer_constants.h
@@ -64,6 +64,11 @@ const std::string kTimerNameGbaPoseGraphCopy = "gba_pose_graph_copy";
 const std::string kTimerNameMapMergeGbaPoseGraphCopy =
     "map_merge_gba_pose_graph_copy";
 
+const std::string kTimerNamePoseGraphCopyVfs = "pose_graph_copy_vfs";
+const std::string kTimerNamePoseGraphCopyPoses = "pose_graph_copy_poses";
+const std::string kTimerNamePoseGraphCopyObjs = "pose_graph_copy_objs";
+const std::string kTimerNamePoseGraphCopySetUpdatedVals = "pose_graph_copy_set_updated";
+
 const std::string kTimerNameIterationLoggerWrite = "iteration_logger_write";
 const std::string kTimerNameLbaOptLoggerWrite = "lba_opt_logger_write";
 const std::string kTimerNameGbaOptLoggerWrite = "gba_opt_logger_write";
